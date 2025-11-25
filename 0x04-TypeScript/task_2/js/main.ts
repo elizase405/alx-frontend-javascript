e05e596c3c36a1c430a338913b77307a3e6f4a90
Elizabeth <elizase405@gmail.com>
2025-11-24 22:13:41 -0800

    feat: Advanced types Part 1

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,40 @@
+Interface DirectorInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workDirectorTasks(): string;
+}
+
+Interface TeacherInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface:
+	workFromHome(): string {
+		return "Working from home";
+	}
+	getToWork(): string {
+		return "Getting a coffee break":
+	}
+	workDirectorTasks(): string {
+		return "Getting to director tasks"
+	}
+
+class Teacher implements TeacherInterface:
+	workFromHome(): string {
+		return "Cannot work from home";
+	}
+	getCoffeeBreak(): string {
+		return "Cannot have a break":
+	}
+	workTeacherTasks(): string {
+		return "Getting to work"
+	}
+
+function createEmployee(salary: number | string): Director | Teacher {
+	if (typeof salary === "number" && salary < 500) {
+		return new Teacher()
+	else {
+		return new Director();
+	}
