b2bf2cb96bfa5daeb7b6065362115afafc7c3d8a
Elizabeth <elizase405@gmail.com>
2025-11-24 21:56:53 -0800

    feat: Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,26 @@ interface printTeacherFunction {
 function printTeacher({ firstName, lastName }: printTeacherFunction) {
 	return `${firstName}. ${lastName}`;
 }
+
+interface StudentClassConstructorInterface {
+	new (firstName: string, lastName: string): StudentClassInterface
+}
+interface StudentClassInterface { 
+	workOnHomework(): string;
+	displayName(): string;
+}
+class StudentClass implements StudentClassInterface:
+	private firstName: string;
+	private lastName: string;
+
+	constructor(firstName: string, lastName: string) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	workOnHomework(): string {
+		return "Currently working";
+	}
+	displayName(): string {
+		return this.firstName;
+	}
+
